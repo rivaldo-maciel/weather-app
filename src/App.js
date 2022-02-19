f1981db935a096a74c159ffe1d81ee30726e7a14
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-02-19 20:18:01 -0300

    Salva localizacao atual do usuario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [coords, setCoords] = useState();
+
+  useEffect(() => {
+    const getCoords = (coords) => {
+      const { latitude, longitude } = coords.coords;
+      setCoords({ latitude, longitude });
+    }
+      navigator.geolocation.getCurrentPosition(getCoords);
+  }, []);
+
   return (
     <h1>Weather App</h1>
   );
